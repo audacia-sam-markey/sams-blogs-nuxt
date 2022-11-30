d603fc73f4045c8e7a047fbdc248f0aea1fe0d37
audacia-sam-markey <sam.markey@audacia.co.uk>
2022-11-30 15:18:04 +0000

    feat : added local

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,19 @@
   <div>Home page</div>
 </template>
 
-<script setup lang="ts">
-useHead({
-  script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
-});
+<script lang="ts">
+export default {
+  head() {
+    return {
+      script: [
+        { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+      ],
+    };
+  },
+};
+// useHead({
+//   script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+// });
 </script>
 
 <style scoped></style>
