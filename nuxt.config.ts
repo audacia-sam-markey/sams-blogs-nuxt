959e1fe8cde971c54525ef2d6f1161e8501c74d5
audacia-sam-markey <sam.markey@audacia.co.uk>
2022-12-07 12:14:44 +0000

    disabling SSR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 export default defineNuxtConfig({
+  ssr: false,
   modules: ["@nuxt/content"],
   content: {
     documentDriven: true,
   },
+
   generate: {
     exclude: [
       /^\/admin/, // path starts with /admin
